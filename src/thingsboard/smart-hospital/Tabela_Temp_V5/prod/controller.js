4a31900ae54d35e0aff9d2e5b4c349ec7a7f84fd
victorhjoe <43349527+victorhjoe@users.noreply.github.com>
2026-05-19 18:30:41 -0300

    ajuste slot

--- src/thingsboard/smart-hospital/Tabela_Temp_V5/prod/controller.js
+++ src/thingsboard/smart-hospital/Tabela_Temp_V5/prod/controller.js
@@ -3644,7 +3644,7 @@ function _man4BuildReport(allData) {
   var MAN4_SLOT_HOURS = [8, 10, 12, 14, 16, 18, 20];
   var TEMP_LIMIT = 24;
   var nowMs = Date.now();
-  var GRACE_MS = 5 * 60 * 1000;
+  var GRACE_MS = 45 * 60 * 1000;
 
   function fmtDate(d) {
     return String(d.getDate()).padStart(2, '0') + '/' +
@@ -3697,7 +3697,7 @@ function _man4BuildReport(allData) {
       for (var hi = 0; hi < MAN4_SLOT_HOURS.length; hi++) {
         var hourBRT = MAN4_SLOT_HOURS[hi];
         var slotTs = Date.UTC(day.y, day.m, day.d, hourBRT + 3, 0, 0, 0);
-        if (slotTs > nowMs - GRACE_MS && !tsMap[slotTs]) continue;
+        if (slotTs > nowMs - GRACE_MS) continue;
         var row = tsMap[slotTs];
         var status, value;
 
